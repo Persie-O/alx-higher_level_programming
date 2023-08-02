4782657f88cd0639f3942f740319ac167fae2fb2
Pascal <persiewailer697@gmail.com>
2023-08-02 15:59:04 +0300

    fetches character name and dislpay using JQuery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+// script that fetches the character name from this URL:
+// https://swapi-api.hbtn.io/api/people/5/?format=json
+const $ = window.$;
+$(document).ready(function() {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+    $('#character').text(data.name);
+  });
+});
