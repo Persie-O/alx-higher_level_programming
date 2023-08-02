55ab3892435e3b1e8d7b31e785959cc00ac53e38
Pascal <persiewailer697@gmail.com>
2023-08-02 15:41:33 +0300

    changes header color fron red to green color on click using JQuery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+// script that toggles the class of the <header> element when the user
+// clicks on the tag DIV#toggle_header
+const $ = window.$;
+$(document).ready(function() {
+  $('#toggle_header').click(function() {
+    $('header').toggleClass('red green');
+  });
+});
